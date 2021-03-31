3bb9c8243318d6c219c0b787f16f5993d5306f74
dingsheng <727040141@qq.com>
2021-03-31 16:35:23 +0800

    chore: update

--- packages/array/src/unique.ts
+++ packages/array/src/unique.ts
@@ -1,13 +1,7 @@
-const a = 0;
 /**
  * @param {Array} arr
  */
 
-export function unique(arr: Array<any>): Array<any> {
-  // //将数组转化为集合 Set
-  // let set = new Set(arr);
-  // //将 set 展开创建一个数组
-  // let array = [...set];
-  // return array;
+export function unique<T>(arr: Array<T>): Array<T> {
   return [...new Set(arr)];
 }
